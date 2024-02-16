f190c61988ac01bdbac760ff3361baafb463ffaf
Eslam Mahgoub <eslamkhaledtawfik@gmail.com>
2024-02-16 15:02:40 +0100

    Add Item Dashboard

--- src/config/context_processors.py
+++ src/config/context_processors.py
@@ -2,12 +2,16 @@ from django.urls import reverse
 
 def site_urls(request):
   project_create_url = reverse("projects:create")
-  deactivate_project_url = reverse("projects:deactivate_project")
   projects_list_url = reverse("projects:list")
+  item_create_url = reverse("items:create")
+  items_list_url = reverse("items:list")
+  deactivate_project_url = reverse("projects:deactivate_project")
   return {
     "home_url": reverse("home"),
     "about_url": reverse("about"),
     "project_create_url": project_create_url,
     "deactivate_project_url": deactivate_project_url,
     "projects_list_url": projects_list_url,
+    "item_create_url": item_create_url,
+    "items_list_url": items_list_url,
   }
--- src/core/views.py
+++ src/core/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-
+from items.models import Item
 
 def index(request):
-  return render(request, 'core/index.html', {})
+  if not request.user.is_authenticated:
+    return render(request, 'core/index.html', {})
+  
+  qs = Item.objects.filter(project=request.project)
+  return render(request, "dashboard/home.html", {"items_list": qs})
 
 
 def about(request):
